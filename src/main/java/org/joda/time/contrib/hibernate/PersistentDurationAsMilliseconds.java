ca82fd45e5fc54f51ad1b60fc8a0ddf3a14afb58
Josh Masterson <joshua.d.masterson@gmail.com>
2019-02-10 17:40:59 -0600

    <NO JIRA>: added more important comment

--- src/main/java/org/joda/time/contrib/hibernate/PersistentDurationAsMilliseconds.java
+++ src/main/java/org/joda/time/contrib/hibernate/PersistentDurationAsMilliseconds.java
@@ -53,7 +53,7 @@ public class PersistentDurationAsMilliseconds implements UserType, Serializable 
             return null;
         }
 
-        // This comment is important
+        // This comment is more important
         return new Duration(b.longValue());
     }
 
